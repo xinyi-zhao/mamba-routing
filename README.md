2d28fc461e5bfc0e7bc38a58e30d5b45fadc7bce
xinyi-zhao <zhxyqq1999@163.com>
2024-05-29 01:11:32 +0000

    mamba finetune code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 torch version 2.2.0
 
 # Evaluation
+## Tasks
 Three types of tasks:
 ### Common Knowledge QA: 
 - nq_open: wikipedia based knowledge
@@ -22,19 +23,25 @@ python evaluation.py --datasets code2text dialog_summary cnn_news  --limit 1000
 
 python evaluation.py --datasets triviaqa squad swde drop  --limit 100
 
+## Running with finetuned model:
+python evaluation.py --model state-spaces/mamba-370m --checkpoint state-spaces_mamba-370m_nq_open/checkpoint-500 --batch_size 32 --datasets nq_open --limit 100
+
 ## Todo
 - Ensure evaluation runs on gpt-neo, opt, pythia, hybrid h3
 - Add inference latency measurement
 - Be careful about the prompt design. 
 
 # Finetune
+python finetune.py --dataset summarization  --limit 100
+
 ### Sample code used for fine-tuning mamba-chat (/training)
-python train_mamba.py --model state-spaces/mamba-790m --tokenizer EleutherAI/gpt-neox-20b --learning_rate 5e-5 --batch_size 1 --gradient_accumulation_steps 4 --optim paged_adamw_8bit --data_path ./ultrachat_small.jsonl --num_epochs 3
+~~python train_mamba.py --model state-spaces/mamba-790m --tokenizer EleutherAI/gpt-neox-20b --learning_rate 5e-5 --batch_size 1 --gradient_accumulation_steps 4 --optim paged_adamw_8bit --data_path ./ultrachat_small.jsonl --num_epochs 3~~
 
 ## Todo
-- Load dataset (edit /training/data.py to use our dataset)
-- Finetune and save model (see /training)
+- ~Load dataset (edit /training/data.py to use our dataset)~
+- ~Finetune and save model (see /training)~
 - Add training throughput measurement
+- Finetuning all all the tasks
 - Prompt based smaller model? (edit prompt template in composed.py to do in-context learning for each sub-model)
 - decrease the effect of prompt design during evaluation
 
--- loaddata/summarization.py
+++ loaddata/summarization.py
@@ -3,6 +3,8 @@ from datasets import load_metric, load_dataset
 import numpy as np
 from evaluate import load
 import re
+from .finetunedataset import FinetuneDataModumn
+
 def load_summarization_evaluation(name = "nq", limit = -1):
     if(name == "code2text"):
         dataset = load_dataset("google/code_x_glue_ct_code_to_text", "java")
@@ -46,3 +48,47 @@ def load_summarization_evaluation(name = "nq", limit = -1):
                 break
         metrics = [load("rouge")]
         return prompts, labels, metrics
+
+def load_summarization_training(name, tokenizer, limit = -1):
+    prompts = []
+    labels = []
+    if(name == "code2text" or name == "summarization"):
+        dataset = load_dataset("google/code_x_glue_ct_code_to_text", "java")
+        df = dataset["train"].to_pandas() 
+        cnt = 0
+        for _ , row in df.iterrows():
+            prompt = row["original_string"] + "What is the funtion of this code? Answer:"
+            label = row["docstring"]
+            prompts.append(prompt)
+            labels.append(label)
+            cnt += 1
+            if limit > -1 and cnt == limit:
+                break
+    
+    if name == "dialog_summary" or name == "summarization":
+        dataset = load_dataset("knkarthick/dialogsum")
+        df = dataset["train"].to_pandas() 
+        cnt = 0
+        for _ , row in df.iterrows():
+            prompt = "Dialogue:" + row["dialogue"] + "What is the summarization of this dialogue? Answer:"
+            label = row["summary"]
+            prompts.append(prompt)
+            labels.append(label)
+            cnt += 1
+            if limit > -1 and cnt == limit:
+                break
+    
+    if name == "cnn_news" or name == "summarization":
+        dataset = load_dataset("abisee/cnn_dailymail", "3.0.0")
+        df = dataset["train"].to_pandas() 
+        cnt = 0
+        for _ , row in df.iterrows():
+            prompt = "Article: " + row["article"] + "\nWhat is the highlight of this article? Answer:"
+            label = row["highlights"]
+            prompts.append(prompt)
+            labels.append(label)
+            cnt += 1
+            if limit > -1 and cnt == limit:
+                break
+    
+    return FinetuneDataModumn(tokenizer, prompts, labels)
